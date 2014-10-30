4f24767b321e0c47704ad738d1578427685c5694
le-yams <ydisanto@gmail.com>
2014-10-30 01:31:03 +0100

    add jna-libc dependency create classes for unix os information implementation

---
+++ src/main/java/oshi/software/os/unix/UnixOperatingSystem.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) Daniel Doubrovkine, 2010 dblock[at]dblock[dot]org All Rights
+ * Reserved Eclipse Public License (EPLv1) http://oshi.codeplex.com/license
+ */
+package oshi.software.os.unix;
+
+import java.util.HashMap;
+import java.util.Map;
+import jna.platform.unix.LibCWrapper;
+import jna.platform.unix.UNameInfo;
+import lombok.Getter;
+import oshi.software.os.OperatingSystem;
+
+/**
+ * Microsoft Windows is a family of proprietary operating systems most commonly
+ * used on personal computers.
+ *
+ * @author dblock[at]dblock[dot]org
+ */
+public class UnixOperatingSystem implements OperatingSystem {
+
+//  UNameInfo example
+//    sysname=Linux, 
+//    nodename=virtual-debian, 
+//    release=3.2.0-4-amd64, 
+//    version=#1 SMP Debian 3.2.63-2, 
+//    machine=x86_64, 
+//    domainname=(none)
+    
+    @Getter
+    private final String family;
+    
+    @Getter
+    private final String manufacturer;
+    
+    @Getter
+    private final UnixOperatingSystemVersion version;
+
+    public UnixOperatingSystem() {
+        UNameInfo info = LibCWrapper.uname();
+        family = parseFamily(info);
+        manufacturer = parseManufacturer(info);
+        version = UnixOperatingSystemVersion.of(info);
+    }
+
+    private static String parseManufacturer(UNameInfo info) {
+        String manufacturer = manufacturersForSysname.get(info.getSysname());
+        // TODO: handle if null
+        return manufacturer;
+    }
+    
+    private static String parseFamily(UNameInfo info) {
+        String family = familiesForSysname.get(info.getSysname());
+        // TODO: handle if null
+        return family;
+    }
+    
+    private static final Map<String, String> familiesForSysname = new HashMap<String, String>();
+    private static final Map<String, String> manufacturersForSysname = new HashMap<String, String>();
+    static {
+        familiesForSysname.put("Linux", "Linux");
+        familiesForSysname.put("DragonFly", "BSD");
+        familiesForSysname.put("FreeBSD", "BSD");
+        familiesForSysname.put("NetBSD", "BSD");
+        familiesForSysname.put("Darwin", "BSD");
+        familiesForSysname.put("Haiku", "BeOS");
+        familiesForSysname.put("HP-UX", "Unix");
+        familiesForSysname.put("AIX", "Unix");
+        familiesForSysname.put("OS400", "Unix");
+        familiesForSysname.put("IRIX", "Unix");
+        familiesForSysname.put("SunOS", "Unix");
+        familiesForSysname.put("ULTRIX", "Unix");
+        familiesForSysname.put("UnixWare", "Unix");
+        
+        
+        manufacturersForSysname.put("HP-UX", "Hewlett-Packard");
+        manufacturersForSysname.put("AIX", "IBM");
+        manufacturersForSysname.put("OS400", "IBM");
+        manufacturersForSysname.put("IRIX", "Silicon Graphics");
+        manufacturersForSysname.put("Darwin", "Apple");
+        manufacturersForSysname.put("SunOS", "Sun Microsystems");
+        manufacturersForSysname.put("ULTRIX", "Digital Equipment Corporation");
+        manufacturersForSysname.put("UnixWare", "Xinuos");
+        
+    }
+}
---
+++ src/main/java/oshi/software/os/unix/UnixOperatingSystemVersion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oshi.software.os.unix;
+
+import jna.platform.unix.UNameInfo;
+import lombok.Value;
+import oshi.software.os.OperatingSystemVersion;
+
+/**
+ *
+ * @author Yann D'Isanto
+ */
+@Value
+public final class UnixOperatingSystemVersion implements OperatingSystemVersion {
+
+    private final String kernelVersion;
+
+    private final String kernelRelease;
+
+    private final String machine;
+
+    public static UnixOperatingSystemVersion of(UNameInfo info) {
+        return new UnixOperatingSystemVersion(info.version, info.release, info.machine);
+    }
+}
